b337054c672f9c3f87d19ace36875abf309db035
Michael Cluver <michaelcluver@Michaels-MacBook-Air-3.local>
2023-01-12 13:53:45 -0500

    Blank files, pt2

--- strategy/Character.java
+++ strategy/Character.java
@@ -1,4 +1,27 @@
+package strategy;
+
 public abstract class Character 
 {
-    
+    protected String name;
+    protected WeaponBehavior WeaponBehavior;
+
+    public Character (String name)
+    {
+
+    }
+
+    public String attack()
+    {
+
+    }
+
+    public void setWeaponBehavior(WeaponBehavior wb)
+    {
+
+    }
+
+    public String toString()
+    {
+        
+    }
 }
